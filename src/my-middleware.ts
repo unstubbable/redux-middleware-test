29dacd82826a68fe515ab66d5e41e13e3050e513
Hendrik Liebau <mail@hendrik-liebau.de>
2017-10-24 12:24:27 +0200

    Fix middleware type

--- src/my-middleware.ts
+++ src/my-middleware.ts
@@ -1,9 +1,12 @@
 import { Dispatch, Middleware, Store } from 'redux';
 import { AppState } from './types';
 
-export const myMiddleware: Middleware = (store: Store<AppState>) => {
+export const myMiddleware: Middleware = <S = AppState>(store: Store<S>) => {
+  // The current redux typings don't go well with TS >= 2.4.
+  // Therefore we need to declare `action` as `any`. This will improve with
+  // https://github.com/reactjs/redux/pull/2563
   // tslint:disable-next-line no-any
-  return (next: Dispatch<AppState>) => (action: any) => {
+  return (next: Dispatch<S>) => (action: any) => {
     // tslint:disable-next-line no-console
     console.log(action);
     return next(action);
